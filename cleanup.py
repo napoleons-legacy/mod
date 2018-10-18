0ce291a063b5fc93f7f8b6c3f63671149e822ada
nappys-legacy <pepejohn.conquest@gmail.com>
2018-10-18 03:28:06 -0400

    Write a better cleanup program

--- cleanup.py
+++ cleanup.py
@@ -2,6 +2,7 @@ import os
 from typing import List
 
 MOD_DIRECTORY = "Napoleon's Legacy"
+EXTENSIONS = ("txt", "lua", "map", "csv")
 
 def clean_line(line: str) -> str:
     """Clean a line by removing useless information and following one standard.
@@ -10,9 +11,9 @@ def clean_line(line: str) -> str:
     :returns: str -- The cleaner line
     """
 
-    return line.replace("\t", "    ").replace("\r\n", "\n").rstrip("\t ")
+    return line.replace("\t", "    ").replace("\r\n", "\n").rstrip() + "\n"
 
-def clean_file(filepath: str) -> None:
+def clean_file(filepath: str) -> bool:
     """Clean the entire file by replacing tabs with spaces,
     replacing Windows line endings with Unix line endings,
     and removing trailing whitespace. We also adjust the file
@@ -20,8 +21,10 @@ def clean_file(filepath: str) -> None:
 
     :param filepath: The path of the file
     :type filepath: str
+    :returns: bool -- Whether the file was changed or not
     """
 
+    file_changed = False
     with open(filepath, "r+", encoding="ISO-8859-1") as file:
         lines = file.readlines()
         for index, line in enumerate(lines):
@@ -29,33 +32,49 @@ def clean_file(filepath: str) -> None:
             if cleaned.isspace():
                 cleaned = "\n"
 
+            if cleaned != line:
+                file_changed = True
+
             lines[index] = cleaned
 
         # check for more style things
-        clean_style(lines)
+        style_changed = clean_style(lines)
+
+        if style_changed:
+            file_changed = True
 
         # wipe the file and rewrite
         file.seek(0)
         file.truncate()
         file.writelines(lines)
 
-def clean_style(lines: List[str]) -> None:
+        return file_changed
+
+def clean_style(lines: List[str]) -> bool:
     """Adjust the file information to be stylistically cleaner
     :param lines: The file contents
     :type lines: List[str]
+    :returns: bool -- Whether the styling rules were used or not
     """
+
+    style_changed = False
 
     # remove any extra newlines at the start of the file
     while lines and lines[0].isspace():
         del lines[0]
+        style_changed = True
 
     # remove any extra newlines at the end of the file.
     while lines and lines[-1].isspace():
         del lines[-1]
+        style_changed = True
 
     # add a newline to the end of the file if not exists.
     if lines and not lines[-1].endswith("\n"):
-        lines[-1] = lines[-1] + "\n"
+        lines[-1] += "\n"
+        style_changed = True
+
+    return style_changed
 
 def good_ext(filename: str) -> bool:
     """Verify that the filename has a valid file extension
@@ -63,12 +82,23 @@ def good_ext(filename: str) -> bool:
     :type filename: str
     """
 
-    exts = ("." + s for s in ["txt", "lua", "map", "csv"])
+    exts = ("." + s for s in EXTENSIONS)
     return any([good for good in exts if filename.endswith(good)])
 
 if __name__ == '__main__':
+    none_changed = True
+    change_count = 0
+
     for dirpath, dirnames, filenames in os.walk(MOD_DIRECTORY):
         for name in filter(good_ext, filenames):
             filepath = os.path.join(dirpath, name)
-            print(f"Cleaning {filepath}")
-            clean_file(filepath)
+            cleaned = clean_file(filepath)
+            if cleaned:
+                print(f"Cleaned {filepath}")
+                none_changed = False
+                change_count += 1
+
+    if none_changed:
+        print("No files were changed.")
+    else:
+        print(f"{change_count} files were changed.")
